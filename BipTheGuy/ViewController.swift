c05d998fc7c2b7435fa729b27e19c6c79d6ae972
Brishti Saha <42756455+sahabr@users.noreply.github.com>
2021-02-22 14:25:48 -0500

    4.5-alerts

--- BipTheGuy/ViewController.swift
+++ BipTheGuy/ViewController.swift
@@ -32,21 +32,27 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func punchButtonPressed(_ sender: Any) {
-
-        let originalImageFrame = imageView.frame
-        let imageWidthShrink: CGFloat = 20
-        let imageHeightShrink: CGFloat = 20
-        let smallerImageFrame = CGRect( x: imageView.frame.origin.x + imageWidthShrink , y: imageView.frame.origin.y + imageHeightShrink,
-            width: imageView.frame.width - (2 * imageWidthShrink),
-            height: imageView.frame.height - (2 * imageHeightShrink))
-        
-        playSound(name: "punchSound")
-
-        imageView.frame = smallerImageFrame
-        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10.0, options: [], animations: {self.imageView.frame = originalImageFrame})
-        
+    func showAlert(title: String, message: String){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
     }
+    
+    @IBAction func photoOrCameraPressed(_ sender: UIButton) {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) {(_) in print("You clicked Photo Library")}
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) {(_) in print("You clicked Camera")}
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(photoLibraryAction)
+        alertController.addAction(cameraAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+
+    }
+    
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
         
